32621b753bf79cc4e9f14493ad5bf28bf437d990
ruimagro <rmagro@hotmail.com>
2020-01-15 13:51:08 +0000

    added initial dotnet new consoel app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitpod_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
